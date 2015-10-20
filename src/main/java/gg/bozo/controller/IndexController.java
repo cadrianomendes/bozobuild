521a78640f8156dc694ba1efc0e142653f61ecd7
Adriano <c.adrianomendes@gmail.com>
2015-10-20 15:32:17 -0200

    First Commit

---
+++ src/main/java/gg/bozo/controller/IndexController.java
@@ -0,0 +1,23 @@
+package gg.bozo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+    public String index(@RequestParam(value="title", required=false, defaultValue="Título") String title, Model model) {
+        model.addAttribute("title", title);
+        return "index";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
